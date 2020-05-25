a1320f662f1591d058732e65d0dfb4996bdc897f
Maria Rodriguez Torres <mcrodriguezt618@gmail.com>
2020-05-24 23:39:11 -0500

    Testing

---
+++ test/mocks/mdResults.js
@@ -0,0 +1,30 @@
+export const NODE_MD_RESULT = [
+  {
+    href: 'https://www.npmjs.com/package/valid-url',
+    ok: 'fail',
+    path: './mocks/node.md',
+    status: '404',
+    text: 'Documentación node sobre validar urls',
+  },
+];
+
+export const NO_VALIDATE_RESULT = [
+  {
+    href: 'https://www.npmjs.com/package/valid-url',
+    path: './mocks/novalidate.md',
+    text: 'Documentación node sobre validar urls',
+  },
+];
+
+export const DIRECTORY_RESULT_NO_VALIDATE = [
+  {
+    href: 'https://www.npmjs.com/package/valid-url',
+    path: './mocks//node.md',
+    text: 'Documentación node sobre validar urls',
+  },
+  {
+    href: 'https://www.npmjs.com/package/valid-url',
+    path: './mocks//novalidate.md',
+    text: 'Documentación node sobre validar urls',
+  },
+];

89a7b436377d7c0483922e83c7fd2e9945811936
Maria Rodriguez Torres <mcrodriguezt618@gmail.com>
2020-05-25 00:27:41 -0500

    Testing 2

--- test/mocks/utilitiesResults.js
+++ test/mocks/utilitiesResults.js
@@ -1,22 +1,4 @@
-export const NODE_MD_RESULT = [
-  {
-    href: 'https://www.npmjs.com/package/valid-url',
-    ok: 'fail',
-    path: './mocks/node.md',
-    status: '404',
-    text: 'Documentación node sobre validar urls',
-  },
-];
-
-export const NO_VALIDATE_RESULT = [
-  {
-    href: 'https://www.npmjs.com/package/valid-url',
-    path: './mocks/node.md',
-    text: 'Documentación node sobre validar urls',
-  },
-];
-
-export const DIRECTORY_RESULT_NO_VALIDATE = [
+export const MD_EXTRACTOR_RESULT = [
   {
     href: 'https://www.npmjs.com/package/valid-url',
     path: './mocks//node.md',
@@ -25,6 +7,23 @@ export const DIRECTORY_RESULT_NO_VALIDATE = [
   {
     href: 'https://www.npmjs.com/package/valid-url',
     path: './mocks//novalidate.md',
+    text: 'Documentación node sobre validar urls',
+  },
+];
+
+export const MD_EXTRACTOR_VALIDATED = [
+  {
+    href: 'https://www.npmjs.com/package/valid-url',
+    ok: 'fail',
+    path: './mocks//node.md',
+    status: '404',
+    text: 'Documentación node sobre validar urls',
+  },
+  {
+    href: 'https://www.npmjs.com/package/valid-url',
+    ok: 'fail',
+    path: './mocks//novalidate.md',
+    status: '404',
     text: 'Documentación node sobre validar urls',
   },
 ];
